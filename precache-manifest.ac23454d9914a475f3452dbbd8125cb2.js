cc18e5af9a8d2cece202f5b3cdce5663c487eb73
Aleksandr Khalaimov <aleksandrkh@wizardsdev.com>
2022-05-15 22:44:35 +0300

    deploy

---
+++ precache-manifest.ac23454d9914a475f3452dbbd8125cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5670faea66269c6a8c",
+    "url": "/test-task-vue/css/app.804fd3b5.css"
+  },
+  {
+    "revision": "fafff8d215e9b2e1e323",
+    "url": "/test-task-vue/css/chunk-vendors.b75c309f.css"
+  },
+  {
+    "revision": "6773d40f2e8625ddc4f6cc137971f2c4",
+    "url": "/test-task-vue/index.html"
+  },
+  {
+    "revision": "ba5670faea66269c6a8c",
+    "url": "/test-task-vue/js/app.a02ac9a6.js"
+  },
+  {
+    "revision": "fafff8d215e9b2e1e323",
+    "url": "/test-task-vue/js/chunk-vendors.c6b9c299.js"
+  },
+  {
+    "revision": "e6ec8f592b5cfb02adb199eae64419ea",
+    "url": "/test-task-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-task-vue/robots.txt"
+  }
+]);
